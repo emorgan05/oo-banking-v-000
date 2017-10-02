32037a32b6550c01406555f7f777fb591963cc8c
Emily Morgan <emorgan05@gmail.com>
2017-10-02 14:01:26 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if @status == "complete" #&& @status != "reversed"
+    if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
